b801218d68d0b394301d68e3ea0e600b496f3f74
dkbrookie <matthew@dkbinnovative.net>
2018-08-06 10:25:46 -0500

    Added documentation, sanitized
    -Added documentation to better explain script processes.
    -Sanitized all data in the script so this can be public on github, stored all sensitive and client identifying info in txt files on the local server

--- syncPasswordExpiration.ps1
+++ syncPasswordExpiration.ps1
@@ -1,41 +1,33 @@
 <#
 -Summary-
 Function 1)
-This script finds the password expiration dates for all users in AD for the OU you specify in $ADOU, then compares that expiration date to the expiration date in O365.
-If the password is already expired in AD but the account is still active in O365, it will force a password reset on next login for that user in O365, and AD. By
-default, despite the password policy being set in O365, it will only give the user a notification that their password is expired but continue to allow them to use
-their account. With this script, you ensure a user is forced to change their password once it's expired. This does check both AD and O365 separately so if the password
-date is expired in O365 but not AD, it will only force the reset in O365 and the same in reverse.
+This script finds the password expiration dates for all users in AD for the OU you specify in $ADOU, then compares that expiration date to the expiration date in O365. If the password is already expired in AD but the account is still active in O365, it will force a password reset on next login for that user in O365, and AD. By default, despite the password policy being set in O365, it will only give the user a notification that their password is expired but continue to allow them to use their account. With this script, you ensure a user is forced to change their password once it's expired. This does check both AD and O365 separately so if the password date is expired in O365 but not AD, it will only force the reset in O365 and the same in reverse.
 
 Function 2)
 This script also finds accounts in AD that have the "Password Never Expires" box checked and unchecks it, then sets the same property to unchecked in O365.
-
--Notes-
-You'll notice that both the O365 and AD passwords are being reset if even just the O365 password is past expiration. This is becaues without ADFS, O365 doesn't sync
-back to AD; meaning, the password is now different for O365 than AD, and the AD password is still set to the old password. Also, since O365 isn't syncing back to AD,
-potentially the O365 password is newer than AD. By forcing changes in both O365 and AD we keep the force reset days equal in both environments. This ensures that if
-the password is changed in O365, it will HAVE to be changed in AD, too.
 #>
 
-
 ##Define AD user info
-$ADOU = "OU=Users,OU=Surgical Notes,DC=hq,DC=surgicalnotes,DC=com"
+##You can find the OU information for which OU you want to run this script on by opening AD, right clicking the OU you want to run this script on and go to Properties>Attribute Editor>DistinguishedName.
+$ADOU = Get-Content "C:\Windows\LTSvc\ou.txt"
+##Here is where you defines the DistinguishedName -notlike, it's listing the OU "Test Users". If you need to exclude a different OU just swap "Test Users", and you could also add additional OUs to exclude by adding -and $_.DistinguishedName -notlike "*,OU=Another OU Name,*"
 $ADUsers = Get-ADUser -Filter * -SearchBase $ADOU | Where-Object {$_.DistinguishedName -notlike "*,OU=Test Users,*" -and $_.Enabled -eq $True} -EA 0 | Select -ExpandProperty SAMAccountName
 
-##Connect to O365
+##Test for an open O365 session, connect if no session is open.
 $O365Connection = Get-PSSession
 IF($O365Connection -eq $Null){
-    $userName = "dkb_admin@surgicalnotes.com"
+    $userName = Get-Content "C:\Windows\LTSvc\o365User.txt"
     $file = "C:\Windows\LTSvc\o365Cred.txt"
-    $cred = new-object -TypeName System.Management.Automation.PSCredential -argumentlist $userName, (Get-Content $File | ConvertTo-SecureString)
+    $cred = new-object -TypeName System.Management.Automation.PSCredential -argumentlist $userName, (Get-Content $file | ConvertTo-SecureString)
     Import-Module MSOnline
     Connect-MsolService -Credential $cred
     $session = New-PSSession -ConfigurationName Microsoft.Exchange -ConnectionUri https://outlook.office365.com/powershell-liveid/ -Credential $cred -Authentication Basic -AllowRedirection
 }
 
-##Set O365 password policy
+##Set O365/AD password policy
 $expirDayPolicy = 90
 $O365NotificationDays = 14
+##Specify the domain for O365 you want to run this script on
 $domain = "surgicalnotes.com"
 Set-MsolPasswordPolicy -ValidityPeriod $expirDayPolicy -NotificationDays $O365NotificationDays -DomainName $domain
 
@@ -43,8 +35,9 @@ Set-MsolPasswordPolicy -ValidityPeriod $expirDayPolicy -NotificationDays $O365No
 ForEach($ADUser in $ADUsers){
     $365User = "$ADUser@$domain"
 
-    ##Get AD user expiration date
+    ##Get the last date the password was changed in AD
     $ADUserPassSet = Get-ADUser -Filter {Enabled -eq $True -and SamAccountName -eq $ADUser} -Properties PasswordLastSet -EA 0 | Select -ExpandProperty PasswordLastSet
+    ##Get AD user "password never expires" status
     $ADNeverExpire = Get-ADUser -Filter {Enabled -eq $True -and SamAccountName -eq $ADUser} -Properties PasswordNeverExpires -EA 0 | Select -ExpandProperty PasswordNeverExpires
     $today = Get-Date
     ##Get the last date the password was changed in O365
